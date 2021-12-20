c2dcab890dd87b0741539729b50c80556ab9b728
Ricky12Awesome <rickygraveen@gmail.com>
2021-12-20 00:28:31 -0600

    success message for install script

--- extension/install_extention.sh
+++ extension/install_extention.sh
@@ -1,6 +1,7 @@
 if [ -d ~/.config/spicetify/Extensions/ ]; then
   wget https://raw.githubusercontent.com/Ricky12Awesome/spotify_info/main/extension/spotify_info.js -P ~/.config/spicetify/Extensions/
   spicetify config extensions spotify_info.js && spicetify apply
+  echo "Extension has been successfully installed."
 else
   echo "Extensions directory was not found, do you have Spicetify installed? install it here https://spicetify.app/docs/getting-started/installation"
 fi
