ffaea5f0c7383e032f2bd12867f0a9b7526644ec
Ricky12Awesome <rickygraveen@gmail.com>
2021-12-20 00:37:57 -0600

    fixed install script

--- extension/install_extention.sh
+++ extension/install_extention.sh
@@ -1,7 +1,13 @@
 if [ -d ~/.config/spicetify/Extensions/ ]; then
-  wget https://raw.githubusercontent.com/Ricky12Awesome/spotify_info/main/extension/spotify_info.js -P ~/.config/spicetify/Extensions/
-  spicetify config extensions spotify_info.js && spicetify apply
-  echo "Extension has been successfully installed."
+  if [ -f ~/.config/spicetify/Extensions/spotify_info.js ]; then
+    wget https://raw.githubusercontent.com/Ricky12Awesome/spotify_info/main/extension/spotify_info.js -P ~/.config/spicetify/Extensions/
+    spicetify config extensions spotify_info.js && spicetify apply
+    echo "Extension has been successfully been installed."
+  else
+    rm ~/.config/spicetify/Extensions/spotify_info.js
+    spicetify config extensions -spotify_info.js && spicetify apply
+    echo "Extension has been successfully been uninstalled."
+  fi
 else
   echo "Extensions directory was not found, do you have Spicetify installed? install it here https://spicetify.app/docs/getting-started/installation"
 fi
