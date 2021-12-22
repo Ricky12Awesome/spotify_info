7388f1b2e7527dc71c0c6edeb78377fbddc727b5
Ricky12Awesome <rickygraveen@gmail.com>
2021-12-21 21:41:31 -0600

    Revert "lifetimes are pain"
    This reverts commit 0ad1e3ac1a057c8de9b12ea9ec23ba3016d6fd69.

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,10 @@
 //! More information can be found on https://github.com/Ricky12Awesome/spotify_info
 
 use std::fmt::{Display, Formatter};
-use std::io::Read;
 use std::iter::FilterMap;
-use std::rc::Rc;
-use std::sync::{Arc, RwLock};
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::net::{Incoming, TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
 
-use mio::net::{TcpListener, TcpStream};
-use mio::{Events, Interest, Poll, Token};
 use tungstenite::{accept, HandshakeError, Message, ServerHandshake, WebSocket};
 use tungstenite::handshake::server::NoCallback;
 use tungstenite::protocol::CloseFrame;
@@ -21,14 +17,11 @@ use tungstenite::protocol::frame::coding::CloseCode;
 //region errors
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
-pub type HandshakeError2 = HandshakeError<ServerHandshake<TcpStream, NoCallback>>;
-
 #[derive(Debug)]
 pub enum Error {
-  Handshake(HandshakeError2),
+  Handshake(HandshakeError<ServerHandshake<TcpStream, NoCallback>>),
   Tcp(std::io::Error),
   Message(MessageError),
-  ClosingError,
 }
 
 #[derive(Debug)]
@@ -37,7 +30,7 @@ pub enum MessageError {
   Other(tungstenite::Error),
 }
 
-impl<'a> Display for Error {
+impl Display for Error {
   fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
     match self {
       Error::Handshake(err) => std::fmt::Display::fmt(err, f),
@@ -59,8 +52,8 @@ impl std::error::Error for Error {}
 
 impl std::error::Error for MessageError {}
 
-impl From<HandshakeError2> for Error {
-  fn from(err: HandshakeError2) -> Self {
+impl From<HandshakeError<ServerHandshake<TcpStream, NoCallback>>> for Error {
+  fn from(err: HandshakeError<ServerHandshake<TcpStream, NoCallback>>) -> Self {
     Self::Handshake(err)
   }
 }
@@ -89,11 +82,6 @@ impl From<tungstenite::Error> for MessageError {
   }
 }
 //endregion errors
-
-/// Server token for mio events
-const SERVER: Token = Token(0);
-/// Client token for mio events
-const CLIENT: Token = Token(1);
 
 /// The state of the track weather it's **Playing**, **Paused** or **Stopped**
 ///
@@ -145,6 +133,8 @@ pub struct Info {
   pub background_url: Option<String>,
 }
 
+type FilterMapFn = fn(std::io::Result<TcpStream>) -> Option<WebSocket<TcpStream>>;
+
 /// Listens to incoming messages from spotify to
 /// get information about the currently playing track
 ///
@@ -155,30 +145,29 @@ pub struct Info {
 /// https://github.com/Ricky12Awesome/spotify_info#installuninstall-spicetify-extension
 pub struct Listener {
   listener: TcpListener,
-  poll: Poll,
-  events: Events,
-  handle: Handle,
+  handle: Arc<Mutex<Handle>>,
 }
 
 /// Handles the current connection
-#[derive(Default, Clone)]
+#[derive(Default)]
 pub struct Handle {
-  is_closed: Arc<AtomicBool>,
+  is_closed: bool,
+  current_connection: Option<WebsocketConnection>,
 }
 
 impl Handle {
-  pub fn new() -> Self {
-    Self::default()
+  pub fn new() -> Arc<Mutex<Self>> {
+    Arc::new(Mutex::new(Self::default()))
   }
 
-  pub fn close(&self) {
-    self.is_closed.store(true, Ordering::SeqCst);
+  pub fn close(&mut self) {
+    self.is_closed = true;
+
+    if let Some(ws) = &mut self.current_connection {
+      ws.close().unwrap();
+    }
   }
 }
-
-unsafe impl Send for Handle {}
-
-unsafe impl Sync for Handle {}
 
 impl Listener {
   /// Creates a [TcpListener] bound on `127.0.0.1:19532`
@@ -187,7 +176,7 @@ impl Listener {
   }
 
   /// Creates a [TcpListener] bound on `127.0.0.1:19532`
-  pub fn new_with_handle(handle: Handle) -> Result<Self, std::io::Error> {
+  pub fn new_with_handle(handle: Arc<Mutex<Handle>>) -> Result<Self, std::io::Error> {
     Self::bind_with_handle(19532, handle)
   }
 
@@ -197,29 +186,19 @@ impl Listener {
   }
 
   /// Creates a [TcpListener] bound on `127.0.0.1` with the given port
-  pub fn bind_with_handle(port: u16, handle: Handle) -> Result<Self, std::io::Error> {
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", port).parse().unwrap())?;
-    let poll = Poll::new()?;
-    let events = Events::with_capacity(128);
-
-    let mut slf = Self { poll, events, listener, handle };
-
-    slf.setup()?;
-
-    Ok(slf)
-  }
-
-  /// Set up polling events for server
-  pub fn setup(&mut self) -> Result<(), std::io::Error> {
-    self.poll.registry().register(&mut self.listener, SERVER, Interest::READABLE)
+  pub fn bind_with_handle(port: u16, handle: Arc<Mutex<Handle>>) -> Result<Self, std::io::Error> {
+    TcpListener::bind(("127.0.0.1", port)).map(|listener| Self {
+      listener,
+      handle,
+    })
   }
 
   /// Iterates for every message, waits until it finds a connection, if that connection closes
   /// it will wait for another connection and so on.
   /// to stop this from iterating use [Self::close]
   /// or set the close handle to true
-  pub fn incoming(&mut self) -> Result<ListenerIter, std::io::Error> {
-    ListenerIter::from(self)
+  pub fn incoming(&self) -> Result<ListenerIter, std::io::Error> {
+    ListenerIter::from(&self.listener, self.handle.clone())
   }
 
   /// Closes the listener and will make [Self::incoming] stop iterating
@@ -228,106 +207,74 @@ impl Listener {
 
 /// Handles the tcp listener and any incoming messages
 pub struct ListenerIter<'a> {
-  listener: &'a mut Listener,
-  stream: Option<TcpStream>,
-  connection: Option<WebsocketConnection<'a>>,
+  incoming: FilterMap<Incoming<'a>, FilterMapFn>,
+  handle: Arc<Mutex<Handle>>,
 }
 
 impl<'a> ListenerIter<'a> {
-  pub fn from(listener: &'a mut Listener) -> Result<Self, std::io::Error> {
-    Ok(Self { listener, stream: None, connection: None })
+  pub fn from(server: &'a TcpListener, handle: Arc<Mutex<Handle>>) -> Result<Self, std::io::Error> {
+    let filter: FilterMapFn = |it| accept(it.ok()?).ok();
+    let incoming = server.incoming().filter_map(filter);
+
+    Ok(
+      Self {
+        incoming,
+        handle,
+      }
+    )
   }
 }
 
-impl<'a> Iterator for &'a mut ListenerIter<'a> {
+impl<'a> Iterator for ListenerIter<'a> {
   type Item = Result<Info, MessageError>;
 
   fn next(&mut self) -> Option<Self::Item> {
-    self.listener.poll.poll(&mut self.listener.events, None).ok()?;
+    let mut result = None;
 
-    let should_close = || self.listener.handle.is_closed.load(Ordering::SeqCst);
+    loop {
+      let mut handle = self.handle.lock().unwrap();
 
-    for event in &self.listener.events {
-      match event.token() {
-        SERVER => {
-          loop {
-            if should_close() {
-              break;
-            }
-
-            let stream = self.listener.listener.accept().ok();
-
-            if stream.is_none() {
-              continue;
-            }
-
-            let stream = stream.unwrap().0;
-
-            self.stream = Some(stream);
-
-            break;
-          }
-
-          if let Some(stream) = &mut self.stream {
-            self.listener.poll.registry()
-              .register(stream.by_ref(), CLIENT, Interest::READABLE | Interest::WRITABLE).ok()?;
-
-            let mut socket = None;
-
-            loop {
-              if socket.is_none() {
-                socket = accept(stream as &TcpStream).ok();
-                continue;
-              }
-
-              break;
-            }
-
-            let connection = WebsocketConnection::new(socket.unwrap());
-            self.connection = Some(connection);
-          }
-        }
-
-        CLIENT => {
-          let mut result = None;
-
-          loop {
-            if !event.is_readable() || event.is_read_closed() || should_close() {
-              break;
-            }
-
-            if let Some(connection) = &mut self.connection {
-              result = connection.next();
-            }
-
-            if result.is_none() {
-              continue;
-            }
-
-            return result;
-          }
-        }
-        _ => unreachable!("Other tokens was somehow registered")
+      if handle.is_closed {
+        return None;
       }
+
+      if handle.current_connection.is_none() {
+        let ws = self.incoming.next()?;
+        handle.current_connection = Some(WebsocketConnection::new(ws));
+        continue;
+      }
+
+      if let Some(ws) = &mut handle.current_connection {
+        result = ws.next();
+      }
+
+      if result.is_none() && !handle.is_closed {
+        handle.current_connection = None;
+        continue;
+      }
+
+      break;
     }
 
-    None
+    result
   }
 }
 
 /// Handles incoming messages from a websocket
-pub struct WebsocketConnection<'a> {
-  socket: WebSocket<&'a TcpStream>,
+pub struct WebsocketConnection {
+  socket: WebSocket<TcpStream>,
 }
 
-impl<'a> WebsocketConnection<'a> {
-  pub fn new(socket: WebSocket<&'a TcpStream>) -> Self {
+impl WebsocketConnection {
+  pub fn new(socket: WebSocket<TcpStream>) -> Self {
     Self {
       socket,
     }
   }
 
   pub fn close(&mut self) -> Result<()> {
+    self.socket.write_message(Message::Text("Closing".to_string()))?;
+
     Ok(self.socket.close(Some(CloseFrame {
       code: CloseCode::Away,
       reason: "Server is closing".into(),
@@ -335,7 +282,7 @@ impl<'a> WebsocketConnection<'a> {
   }
 }
 
-impl<'a> Iterator for WebsocketConnection<'a> {
+impl Iterator for WebsocketConnection {
   type Item = Result<Info, MessageError>;
 
   fn next(&mut self) -> Option<Self::Item> {
